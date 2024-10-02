4e0e0760f1bb42c5dc6ca87ea8408a6c0630f51f
manan75 <4696.stkabirdin@gmail.com>
2024-10-02 17:32:12 +0530

    made a jouney componenet

--- paceWebsite/src/Age8to11.jsx
+++ paceWebsite/src/Age8to11.jsx
@@ -1,10 +1,12 @@
 import NavbarComponent from "./NavbarComponent"
+import Kids1 from "./Kids1"
 
 function Age8to11(){
     return(
         <>
         <NavbarComponent/>
-        <h1>this is page of age grp 8 to 11</h1></>
+        <Kids1/>
+        </>
     )
 }
 
--- paceWebsite/src/App.jsx
+++ paceWebsite/src/App.jsx
@@ -4,17 +4,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import HomePage from './HomePage';
 
 import NasaMap from './NasaMap'
-import Bg from './Bg';
+import Bg from './JourneyComponent.jsx';
 import {PrimaryButton,DangerButton,SecondaryButton,SuccessButton} from './BootstrapComponents/ButtonTypes';
-import bg from './Bg';
+import bg from './JourneyComponent.jsx';
 import Kids1 from './Kids1.jsx';
+
 function App() {
   
 
   return (
     <>
-     <HomePage/> 
-     <NasaMap/> 
+    <HomePage/>
+    <NasaMap/>
     {/* <Bg/> */}
     
 
