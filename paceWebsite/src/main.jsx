7b05bb981028c01ae6db5755c81541270d19da6d
manan75 <4696.stkabirdin@gmail.com>
2024-10-02 20:47:16 +0530

    Made age11to15 page

--- paceWebsite/src/main.jsx
+++ paceWebsite/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import About from './About.jsx'
 import Age8to11 from './Age8to11.jsx'
+import Age11to15Chap from './Age11to15Chap.jsx'
 import './index.css'
 import ReactDOM from 'react-dom/client'
 import {createBrowserRouter, RouterProvider} from "react-router-dom"
@@ -23,6 +24,10 @@ const router = createBrowserRouter([
   {
     path: "/Age8To11",
     element:<Age8to11/>
+  },
+  {
+    path:"Age12to15",
+    element:<Age11to15Chap/>
   }
 ])
 
